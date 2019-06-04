379d8164513d26eea3f61c02a2935ad7105417d8
Carmen Alvarez <calvarez@libon.com>
2019-06-04 10:36:22 +0200

    Add two identical tests to verify that the ProcessLifecycleOwner state is not persisted across tests

--- app/src/test/java/com/example/processlifecycleownerpersistedstate/ExampleUnitTest.kt
+++ app/src/test/java/com/example/processlifecycleownerpersistedstate/ExampleUnitTest.kt
@@ -30,10 +30,25 @@ class ExampleUnitTest {
     }
 
     @Test
-    fun testLaunchActivity() {
+    fun testLaunchActivity1() {
+        testLaunchActivity()
+    }
+
+    @Test
+    fun testLaunchActivity2() {
+        testLaunchActivity()
+    }
+
+    private fun testLaunchActivity() {
+        assertEquals(Lifecycle.State.CREATED, ProcessLifecycleOwner.get().lifecycle.currentState)
         val controller = Robolectric.buildActivity(MainActivity::class.java)
             .create().start().resume().visible()
         assertEquals(Lifecycle.State.RESUMED, ProcessLifecycleOwner.get().lifecycle.currentState)
+        // Note: if we comment out the following 3 lines, and also comment out our tearDown(), testLaunchActivity1()
+        // will pass, but testLaunchActivity2() will fail, because the ProcessLifecycleOwner's state at the
+        // beginning of the second test will be RESUMED instead of CREATED (left-over from the end of the first test).
         controller.pause().stop().destroy()
+        Robolectric.flushForegroundThreadScheduler()
+        assertEquals(Lifecycle.State.CREATED, ProcessLifecycleOwner.get().lifecycle.currentState)
     }
 }
