dedf02d10dfd0b6c96616317a3d0ffe7b559b7f8
Carmen Alvarez <calvarez@libon.com>
2019-06-04 10:15:07 +0200

    Add simple robolectric test, using 4.2.1, which just launches the activity

--- app/src/test/java/com/example/processlifecycleownerpersistedstate/ExampleUnitTest.kt
+++ app/src/test/java/com/example/processlifecycleownerpersistedstate/ExampleUnitTest.kt
@@ -3,15 +3,21 @@ package com.example.processlifecycleownerpersistedstate
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testLaunchActivity() {
+        val controller = Robolectric.buildActivity(MainActivity::class.java)
+            .create().start().resume().visible()
+        controller.pause().stop().destroy()
     }
 }
